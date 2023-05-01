faa167fbe507eee3b154059732f3d0ce7ffe31a7
== <thawhtooin@gmail.com>
2023-05-01 20:24:38 +0630

    finished the video 1

--- admin/index.php
+++ admin/index.php
@@ -1,25 +1,34 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="../css/admin.css">
-  </head>
-  <body>
-    <div class="menu">
-      <div class="wrapper">
-        Home Goes Here
-      </div>
-    </div>
+
+<?php
+include 'partials/menu.php';
+?>
+
     <div class="main-content">
       <div class="wrapper">
-        Main Content Goes Here
+        <h1>Dashboard</h1>
+
+        <div class="col-4 text-center">
+          <h1>5</h1>
+          Categories
+        </div>
+        <div class="col-4 text-center">
+          <h1>5</h1>
+          Categories
+        </div>
+        <div class="col-4 text-center">
+          <h1>5</h1>
+          Categories
+        </div>
+        <div class="col-4 text-center">
+          <h1>5</h1>
+          Categories
+        </div>
+        <div class="clearfix">
+
+        </div>
       </div>
     </div>
-    <div class="footer">
-      <div class="wrapper">
-        <p class="text-center">2020 alright reserved, Some Restaurant. Developed By - <a href="#">Tommy</a></p>
-      </div>
-    </div>
-  </body>
-</html>
+
+    <?php
+    include 'partials/footer.php';
+    ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+  <div class="wrapper">
+    <p class="text-center">2020 alright reserved, Food House. Developed By - <a href="#">Tommy</a></p>
+  </div>
+</div>
+</body>
+</html>
